f6adc272f438f7b402b2a4558bff3b53a930d76b
Chris L8 <ekpyroticfrood@gmail.com>
2019-09-21 20:48:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const config = {
+  type: Phaser.AUTO, // Which renderer to use
+  width: 800, // Canvas width in pixels
+  height: 600, // Canvas height in pixels
+  parent: 'game-container', // ID of the DOM element to add the canvas to
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  // Runs once, loads up assets like images and audio
+}
+
+function create() {
+  // Runs once, after all assets in preload are loaded
+}
+
+function update(time, delta) {
+  // Runs once per frame for the duration of the scene
+}
