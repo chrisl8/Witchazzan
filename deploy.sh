ad89b649d24c906598e9b19b4055ca628aa54e7f
Chris L8 <ekpyroticfrood@gmail.com>
2019-09-29 20:04:46 -0500

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This one script will start up the entire robot with web interface!
+export NVM_DIR="${HOME}/.nvm"
+# shellcheck source=/home/chrisl8/.nvm/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we
+need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+# echo "${SCRIPTDIR}" # For debugging
+
+npm run build
