cdcbf2c39773ac6422422758590ba4d26a8e4d7f
Chris L8 <ekpyroticfrood@gmail.com>
2019-10-15 16:01:40 -0500

    Do not show reconnect text if we never connected to begin with.
    This is just sad and annoying, not to mention the text may overlap
    itself and look bad.
    We can do better than that.
    We can do SO MUCH BETTER THAN THAT!
    
    ... Hello?

--- src/cleanUpAfterDisconnect.js
+++ src/cleanUpAfterDisconnect.js
@@ -1,7 +1,9 @@
 import playerObject from './playerObject';
 
 function cleanUpAfterDisconnect() {
-  playerObject.sceneText.reconnectingText.shouldBeActiveNow = true;
+  if (!playerObject.sceneText.connectingText.shouldBeActiveNow) {
+    playerObject.sceneText.reconnectingText.shouldBeActiveNow = true;
+  }
   // Since most keyboard input is ignored when not connected,
   // we don't want people to be stuck running or firing while
   // they are offline.
