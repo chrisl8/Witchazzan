68d85e54d55cedc8390038f6dfb047a01ac928dc
Chris L8 <ChristenLofland@gmail.com>
2019-11-02 19:21:03 -0500

    Store player name in local storage.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* globals localStorage:true */
 // https://what-problem-does-it-solve.com/webpack/css.html
 import './css/styles.css';
 
@@ -28,21 +29,28 @@ import wait from './utilities/wait';
         'keyup',
         checkForEnterKey,
       );
+      localStorage.setItem('playerName', playerNameInputValue);
       playerObject.playerName = playerNameInputValue;
     }
   };
   if (!playerObject.playerName) {
-    playerObject.domElements.playerNameSubmitButton.addEventListener(
-      'click',
-      addPlayerNameToPlayerObject,
-    );
-    playerObject.domElements.playerNameInputBox.addEventListener(
-      'keyup',
-      checkForEnterKey,
-    );
-    document.getElementById('loading_text').hidden = true;
-    document.getElementById('player_name_entry_div').hidden = false;
-    playerObject.domElements.playerNameInputBox.focus();
+    // Check local storage to see if we already have a name.
+    const existingPlayerName = localStorage.getItem('playerName');
+    if (!existingPlayerName) {
+      playerObject.domElements.playerNameSubmitButton.addEventListener(
+        'click',
+        addPlayerNameToPlayerObject,
+      );
+      playerObject.domElements.playerNameInputBox.addEventListener(
+        'keyup',
+        checkForEnterKey,
+      );
+      document.getElementById('loading_text').hidden = true;
+      document.getElementById('player_name_entry_div').hidden = false;
+      playerObject.domElements.playerNameInputBox.focus();
+    } else {
+      playerObject.playerName = existingPlayerName;
+    }
 
     while (!playerObject.playerName) {
       // eslint-disable-next-line no-await-in-loop
