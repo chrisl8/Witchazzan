b9188458fd870ac0bde4fc3c0384cd485b0e6926
Christen Lofland <ChristenLofland@gmail.com>
2020-06-16 19:19:38 -0500

    Force reload.

--- src/objects/playerObject.js
+++ src/objects/playerObject.js
@@ -8,7 +8,7 @@
  *
  */
 const playerObject = {
-  helpTextVersion: 10, // Increment this to force players to see the Instructions again.
+  helpTextVersion: 11, // Increment this to force players to see the Instructions again.
   // This is the scene you start the game in,
   // and go to if a non-existent scene is requested,
   // and if you press the 'o' key.
