e8b22abf74999b2a55f6ead8ca291bc1c8079690
Christen Lofland <Christen@Lofland.net>
2022-04-08 08:53:21 -0500

    Simplify Hadrons Map export file.

--- client/src/objects/hadrons.js
+++ client/src/objects/hadrons.js
@@ -1,6 +1,5 @@
 /*
- * This is an empty object for holding the game pieces list
+ * This is an empty Map for holding the game pieces list
  * from the server.
  */
-const hadrons = new Map();
-export default hadrons;
+export default new Map();
