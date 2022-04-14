1d8bf7d144d1b2bec04c18ac5c9bf75cbdd93c76
Christen Lofland <Christen@Lofland.net>
2022-04-14 16:17:07 -0500

    Add colors to terminal output from the devUpdate.sh script to help tired people with smol fonts.

--- devUpdate.sh
+++ devUpdate.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+#PURPLE='\033[0;35m'
+YELLOW='\033[1;33m'
+LIGHTCYAN='\033[1;36m'
+#LIGHTBLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+BRIGHT_WHITE='\033[1;97m'
+NC='\033[0m' # NoColor
+
 if ! (command -v git >/dev/null 2>&1); then
-    echo "git is not installed"
+    printf "\n${LIGHT_PURPLE}git is not installed!${NC}\n"
     exit 1
 fi
 
 if ! (command -v node >/dev/null 2>&1); then
-    echo "node is not installed"
-    echo "I recommend that you use nvm:"
-    echo "https://github.com/nvm-sh/nvm"
-    echo ""
-    echo "wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash"
-    echo "nvm install --lts"
+    printf "\n${LIGHT_PURPLE}node is not installed${NC}\n"
+    printf "I recommend that you use nvm:\n"
+    printf "https://github.com/nvm-sh/nvm\n"
+    printf "\n"
+    printf "${LIGHTCYAN}wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash${NC}\n"
+    printf "${LIGHTCYAN}nvm install --lts${NC}\n"
     exit 1
 fi
 
 if ! (node -v | grep "v16" > /dev/null); then
-  echo "Node 16 (LTS) is required"
-  echo "Use node -v to check your version"
-  echo "and update your node version."
-  echo ""
-  echo "If you are using nvm, then run this to update node:"
-  echo "nvm install --lts"
+  printf "\n${LIGHT_PURPLE}Node 16 (LTS) is required${NC}\n"
+  printf "Use node -v to check your version\n"
+  printf "and update your node version.\n"
+  printf "\n"
+  printf "If you are using nvm, then run this to update node:\n"
+  printf "${LIGHTCYAN}nvm install --lts${NC}\n"
   exit 1
 fi
 
+printf "\n${YELLOW}[Installing pm2 for running Dev Server]${NC}\n"
 npm i -g pm2
 
 # Grab and save the path to this script
@@ -39,32 +51,34 @@ SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 # echo "${SCRIPT_DIR}" # For debugging
 
 cd "${SCRIPT_DIR}" || exit
-echo "Pulling latest changes from the GitHub repo:"
+printf "\n${YELLOW}[Pulling latest changes from the GitHub repo]${NC}\n"
 git pull
 cd "${SCRIPT_DIR}/server" || exit
 echo ""
-echo "Installing dependencies for server"
+printf "\n${YELLOW}[Installing dependencies for server]${NC}\n"
 npm ci
 cd "${SCRIPT_DIR}/client" || exit
 echo ""
-echo "Installing dependencies for client"
+printf "\n${YELLOW}[Installing dependencies for client]${NC}\n"
 npm ci
 
-echo ""
-echo "----------------------------------------------"
-echo "For development open two terminals windows:"
-echo ""
-echo "In the first run:"
-echo "cd ${SCRIPT_DIR}/server"
-echo "pm2-dev server.js"
-echo ""
-echo "This will start the server, and restart it when file changes are made."
-echo "It is the same as running 'node server.js', except that it will auto restart when you update your code."
-echo ""
-echo "In the second run:"
-echo "cd ${SCRIPT_DIR}/client"
-echo "npm run start"
-echo ""
-echo "This will build the client as well as rebuild and cause a browser refresh when file changes are made."
-echo "It should also automatically open your web browser to the page."
-echo "If not, go to http://localhost:3001"
+printf "\n"
+printf "${BRIGHT_WHITE}----------------------------------------------${NC}\n"
+printf "For development open two terminals windows:\n"
+printf "\n"
+printf "In the first run:\n"
+printf "${LIGHTCYAN}cd ${SCRIPT_DIR}/server${NC}\n"
+printf "${LIGHTCYAN}pm2-dev server.js${NC}\n"
+printf "\n"
+printf "This will start the server, and restart it when file changes are made.\n"
+printf "It is the same as running 'node server.js', except that it will auto restart when you update you\nr code."
+printf "\n"
+printf "\n"
+printf "${BRIGHT_WHITE}------------------${NC}\n"
+printf "In the second run:\n"
+printf "${LIGHTCYAN}cd ${SCRIPT_DIR}/client${NC}\n"
+printf "${LIGHTCYAN}npm run start${NC}\n"
+printf "\n"
+printf "This will build the client as well as rebuild and cause a browser refresh when file changes ar\ne made."
+printf "It should also automatically open your web browser to the page.\n"
+printf "If not, go to http://localhost:3001\n"
