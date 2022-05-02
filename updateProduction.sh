0845f1b6e4fa954703effcea7b5f9e318e72b756
Christen Lofland <Christen@Lofland.net>
2022-05-02 11:31:42 -0500

    Bugfix: Production update script weird line break.

--- updateProduction.sh
+++ updateProduction.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export NVM_DIR="${HOME}/.nvm"
-[ -s "$NVM_DIR/nvm.sh"   ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Grab and save the path to this script
 # http://stackoverflow.com/a/246128
@@ -8,8 +8,7 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we
-need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 # echo "${SCRIPT_DIR}" # For debugging
