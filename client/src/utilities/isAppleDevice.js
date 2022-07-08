afc8ccaffdf6561a966ac85bcab3dfd70b81f032
Christen Lofland <Christen@Lofland.net>
2022-07-07 22:19:59 -0500

    Detect modern iPads.

--- client/src/utilities/isAppleDevice.js
+++ client/src/utilities/isAppleDevice.js
@@ -1,2 +1,6 @@
 /* globals navigator:true */
-export default /iPhone|iPad|iPod/i.test(navigator.userAgent);
+export default /iPhone|iPad|iPod/i.test(navigator.userAgent) ||
+  // Apple wants the latest iPads to just be "desktops", but of course, they have touch screens and no keyboard.
+  (navigator.maxTouchPoints &&
+    navigator.maxTouchPoints > 2 &&
+    /MacIntel/.test(navigator.platform));
--- client/src/utilities/isMobileBrowser.js
+++ client/src/utilities/isMobileBrowser.js
@@ -1,7 +1,13 @@
 /* globals navigator:true */
-
+// Most mobile devices, other than the newer iPads:
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const isMobileBrowser =
-  isMobile || (navigator.userAgentData && navigator.userAgentData.mobile);
+  isMobile ||
+  // Some devices now return a userAgentData.mobile
+  (navigator.userAgentData && navigator.userAgentData.mobile) ||
+  // Apple wants the latest iPads to just be "desktops", but of course, they have touch screens and no keyboard.
+  (navigator.maxTouchPoints &&
+    navigator.maxTouchPoints > 2 &&
+    /MacIntel/.test(navigator.platform));
 
 export default isMobileBrowser;
