b247867a4b83617cdd9284c4c8382844cf01c6ec
Christen Lofland <Christen@Lofland.net>
2022-07-26 09:10:11 -0500

    Comments

--- client/src/gameLoopAndSceneFactory.js
+++ client/src/gameLoopAndSceneFactory.js
@@ -240,7 +240,7 @@ const gameLoopAndSceneFactory = ({
     // });
 
     // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
-    // collision shapes. In the .json file, there's an object layer with a point named "Spawn Point"
+    // collision shapes. In the .json file, there's an object layer with a point named "Default Spawn Point"
     let spawnPoint = map.findObject(
       'Objects',
       (obj) => obj.name === 'Default Spawn Point',
