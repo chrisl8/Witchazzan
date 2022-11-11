274bc429cf2625f18f33bd0d825f40db6bf46d5b
Christen Lofland <Christen@Lofland.net>
2022-11-11 11:18:10 -0600

    Refactor NPC behavior a bit and add ability to follow paths.

--- client/src/gameLoopFunctions/npcBehavior.js
+++ client/src/gameLoopFunctions/npcBehavior.js
@@ -3,10 +3,11 @@ import hadrons from '../objects/hadrons.js';
 import playerObject from '../objects/playerObject.js';
 import castSpell from '../castSpell.js';
 import clientSprites from '../objects/clientSprites.js';
-import calculateVelocityFromDirection from '../utilities/calculateVelocityFromDirection.js';
 import getSpawnPointFromMap from '../utilities/getSpawnPointFromMap.js';
 import sendDataToServer from '../sendDataToServer.js';
-import makeRandomNumber from '../../../server/utilities/makeRandomNumber.js';
+import paths from '../objects/paths.js';
+import calculateDirectionToLocation from '../utilities/calculateDirectionToLocation.js';
+import moveSpriteTowardLocation from '../utilities/moveSpriteTowardLocation.js';
 
 function npcBehavior(delta, sceneName, map) {
   hadrons.forEach((hadron, key) => {
@@ -23,6 +24,28 @@ function npcBehavior(delta, sceneName, map) {
       // Feel free to exclude a given hadron.sub from these more generic checks at the top.
 
       let hadronUpdated = false;
+
+      const movementPriority = new Map();
+      const movementBehaviorObject = {
+        active: false,
+        move: false,
+        stop: true,
+        stopWhileRotating: false,
+        destination: {
+          x: null,
+          y: null,
+        },
+      };
+      movementPriority.set('followRaycastTarget', {
+        ...movementBehaviorObject,
+      });
+      movementPriority.set('proceedToWaypoint', {
+        ...movementBehaviorObject,
+      });
+      movementPriority.set('stop', {
+        ...movementBehaviorObject,
+      });
+
       const newHadronData = { ...hadron };
 
       let rayCastFoundTarget = false;
@@ -181,64 +204,18 @@ function npcBehavior(delta, sceneName, map) {
                   // Always focus on the nearest target.
                   currentTargetDistance = distance;
 
-                  // Rotate toward it
-                  // TODO: Only rotate if the NPC has this feature.
-                  const newAngleRad = Phaser.Math.Angle.Between(
-                    hadron.x,
-                    hadron.y,
-                    entry.x,
-                    entry.y,
-                  );
-                  // Deal with angles that are physically close to each other but cross boundaries,
-                  // making them numerically distant
-                  // This should work regardless of where that boundary is.
-                  // https://stackoverflow.com/a/28037434/4982408
-                  const newAngleDeg = newAngleRad * Phaser.Math.RAD_TO_DEG;
-                  let difference =
-                    ((newAngleDeg - hadron.dir + 180) % 360) - 180;
-                  difference =
-                    difference < -180 ? difference + 360 : difference;
-
-                  const t = 0.05;
-
-                  let newHadronDir = hadron.dir + t * difference;
-                  // Let Achilles go ahead and catch the Tortoise
-                  if (Math.abs(difference) < 1) {
-                    newHadronDir = newAngleDeg;
-                  }
-                  if (newHadronData.dir !== newHadronDir) {
-                    hadronUpdated = true;
-                    newHadronData.dir = newHadronDir;
-                  }
+                  movementPriority.get('followRaycastTarget').active = true;
+                  movementPriority.get('followRaycastTarget').destination = {
+                    x: entry.x,
+                    y: entry.y,
+                  };
                 }
               }
             }
           });
 
           if (hadron.hasOwnProperty('fol') && hadron.fol) {
-            const clientSprite = clientSprites.get(key);
-            if (clientSprite) {
-              if (rayCastFoundTarget) {
-                const defaultNpcVelocity = 50;
-                clientSprite.sprite.body.setVelocityX(
-                  calculateVelocityFromDirection.x(
-                    hadron.vel || defaultNpcVelocity,
-                    hadron.dir +
-                      (hadron.rvl ? makeRandomNumber.between(-100, 100) : 0),
-                  ),
-                );
-                clientSprite.sprite.body.setVelocityY(
-                  calculateVelocityFromDirection.y(
-                    hadron.vel || defaultNpcVelocity,
-                    hadron.dir +
-                      (hadron.rvl ? makeRandomNumber.between(-100, 100) : 0),
-                  ),
-                );
-              } else {
-                clientSprite.sprite.body.setVelocityX(0);
-                clientSprite.sprite.body.setVelocityY(0);
-              }
-            }
+            movementPriority.get('followRaycastTarget').move = true;
           }
         }
 
@@ -281,7 +258,92 @@ function npcBehavior(delta, sceneName, map) {
           // There is a moment before the client sprite exists.
           clientSprites.get(key).spellCastTimer = spellCastTimer;
         }
+
+        // Path following
+        if (hadron.fph && paths.has(hadron.fph)) {
+          const path = paths.get(hadron.fph);
+          let nextWaypoint;
+          if (hadron.cpd === undefined) {
+            hadronUpdated = true;
+            newHadronData.cpd = 0;
+            nextWaypoint = path.get(0);
+          } else {
+            nextWaypoint = path.get(hadron.cpd);
+          }
+
+          if (nextWaypoint) {
+            movementPriority.get('proceedToWaypoint').active = true;
+            movementPriority.get('proceedToWaypoint').move = true;
+            movementPriority.get('proceedToWaypoint').stop = false;
+            movementPriority.get('proceedToWaypoint').destination = {
+              x: nextWaypoint.x,
+              y: nextWaypoint.y,
+            };
+
+            const closeEnoughProximity = 2;
+            const clientSprite = clientSprites.get(key);
+            if (clientSprite) {
+              if (
+                Math.abs(Math.trunc(hadron.x) - Math.trunc(nextWaypoint.x)) <
+                  closeEnoughProximity &&
+                Math.abs(Math.trunc(hadron.y) - Math.trunc(nextWaypoint.y)) <
+                  closeEnoughProximity
+              ) {
+                // If we are at the destination set next destination in path
+                movementPriority.get('proceedToWaypoint').move = false;
+                movementPriority.get('proceedToWaypoint').stop = true;
+                hadronUpdated = true;
+                newHadronData.cpd++;
+                if (!path.has(newHadronData.cpd)) {
+                  // Reset to 0 if we are at the end of the path.
+                  newHadronData.cpd = 0;
+                }
+              }
+            }
+          }
+        }
       }
+
+      // Movement
+      let done = false;
+      movementPriority.forEach((data) => {
+        if (!done && data.active) {
+          done = data.active;
+          // Rotate toward it
+          // TODO: Only rotate if the NPC has this feature.
+          const newHadronDirection = calculateDirectionToLocation({
+            currentDirection: hadron.dir,
+            fromX: hadron.x,
+            fromY: hadron.y,
+            toX: data.destination.x,
+            toY: data.destination.y,
+            rotationSpeed: hadron.rsp,
+          });
+
+          let rotating = false;
+          if (newHadronData.dir !== newHadronDirection) {
+            rotating = true;
+            hadronUpdated = true;
+            newHadronData.dir = newHadronDirection;
+          }
+
+          if (data.move && (!rotating || !data.stopWhileRotating)) {
+            const clientSprite = clientSprites.get(key);
+            moveSpriteTowardLocation({
+              sprite: clientSprite,
+              currentDirection: hadron.dir,
+              velocity: hadron.vel,
+              randomizeVelocity: hadron.rvl,
+            });
+          } else if (data.stop || (rotating && data.stopWhileRotating)) {
+            const clientSprite = clientSprites.get(key);
+            if (clientSprite) {
+              clientSprite.sprite.body.setVelocityX(0);
+              clientSprite.sprite.body.setVelocityY(0);
+            }
+          }
+        }
+      });
 
       if (hadronUpdated === true) {
         hadrons.set(key, newHadronData);
---
+++ client/src/utilities/calculateDirectionToLocation.js
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+
+function calculateDirectionToLocation({
+  currentDirection,
+  fromX,
+  fromY,
+  toX,
+  toY,
+  rotationSpeed,
+}) {
+  const newAngleRad = Phaser.Math.Angle.Between(fromX, fromY, toX, toY);
+  // Deal with angles that are physically close to each other but cross boundaries,
+  // making them numerically distant
+  // This should work regardless of where that boundary is.
+  // https://stackoverflow.com/a/28037434/4982408
+  const newAngleDeg = newAngleRad * Phaser.Math.RAD_TO_DEG;
+  let difference = ((newAngleDeg - currentDirection + 180) % 360) - 180;
+  difference = difference < -180 ? difference + 360 : difference;
+
+  const defaultRotationSpeed = 1; // 0.05
+  const t = rotationSpeed || defaultRotationSpeed;
+
+  let newDirection = currentDirection + t * difference;
+  // Let Achilles go ahead and catch the Tortoise
+  if (Math.abs(difference) < 1) {
+    newDirection = newAngleDeg;
+  }
+
+  return newDirection;
+}
+
+export default calculateDirectionToLocation;
