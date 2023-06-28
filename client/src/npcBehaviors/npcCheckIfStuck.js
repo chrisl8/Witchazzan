80c4676f4298110fd09cb7fbecd05203b9c5bb3f
Christen Lofland <christen@lofland.net>
2023-06-28 16:23:54 -0500

    Fix and improve sprite stuck detection.

--- client/src/npcBehaviors/npcCheckIfStuck.js
+++ client/src/npcBehaviors/npcCheckIfStuck.js
@@ -3,7 +3,9 @@
 // because there is no reason to transmit this across the network.
 // If control changes while a sprite is stuck, the new controller will discover this on its own.
 // TODO: Make this delta time based, not frame based.
-const stuckThrehold = 20;
+// TODO: Set this per NPC, or as input variables.
+const stuckDurationThreshold = 20;
+const stuckMovementThreshold = 3;
 
 function npcCheckIfStuck(clientSprite) {
   let isStuck = false;
@@ -12,7 +14,9 @@ function npcCheckIfStuck(clientSprite) {
     let stuckY = clientSprite.sprite.getData('stuckY') || 1;
     if (
       !clientSprite.sprite.getData('previousX') ||
-      clientSprite.sprite.getData('previousX') !== clientSprite.sprite.body.x
+      Math.abs(
+        clientSprite.sprite.getData('previousX') - clientSprite.sprite.body.x,
+      ) > stuckMovementThreshold
     ) {
       clientSprite.sprite.setData('previousX', clientSprite.sprite.body.x);
       clientSprite.sprite.setData('stuckX', 0);
@@ -25,9 +29,11 @@ function npcCheckIfStuck(clientSprite) {
 
     if (
       !clientSprite.sprite.getData('previousY') ||
-      clientSprite.sprite.getData('previousY') !== clientSprite.sprite.body.x
+      Math.abs(
+        clientSprite.sprite.getData('previousY') - clientSprite.sprite.body.y,
+      ) > stuckMovementThreshold
     ) {
-      clientSprite.sprite.setData('previousY', clientSprite.sprite.body.x);
+      clientSprite.sprite.setData('previousY', clientSprite.sprite.body.y);
       clientSprite.sprite.setData('stuckY', 0);
     } else {
       if (stuckY) {
@@ -35,13 +41,15 @@ function npcCheckIfStuck(clientSprite) {
       }
       clientSprite.sprite.setData('stuckY', stuckY);
     }
-    isStuck = stuckX > stuckThrehold && stuckY > stuckThrehold;
+    isStuck =
+      stuckX > stuckDurationThreshold && stuckY > stuckDurationThreshold;
   }
   if (isStuck) {
     // Resets so we don't get into rapid loops
     // In other words, take action on a stuck item, but then wait for it to be "stuck again"
     // before taking action again, as some actions are a series.
     clientSprite.sprite.setData('stuckX', 0);
+    clientSprite.sprite.setData('stuckY', 0);
   }
   return isStuck;
 }
