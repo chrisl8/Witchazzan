432ade3baf13885004cda6e288190998a5a7cf4d
Christen Lofland <christen@lofland.net>
2023-07-28 22:21:54 -0500

    Fix fallout logic for sprit collision handler.

--- client/src/gameLoopFunctions/spriteCollisionHandler.js
+++ client/src/gameLoopFunctions/spriteCollisionHandler.js
@@ -88,141 +88,143 @@ function spriteCollisionHandler({
         // Except items and NPCs do not de-spawn when hitting walls.
         sendDataToServer.destroyHadron(spriteKey);
       }
-    } else if (obstacleSpriteKey && hadrons.has(obstacleSpriteKey)) {
-      // Obstacle can linger, and collide, after there hadron is destroyed, but we do not need to act on them anymore.
-      // Sprite to Sprite interactions
-      if (
-        hadrons.get(spriteKey)?.typ === 'message' &&
-        hadrons.get(spriteKey)?.txt !== undefined
-      ) {
-        // Messages - A message sprite that we are tracking has collided with something.
-        if (hadrons.get(obstacleSpriteKey)?.typ === 'player') {
-          // If it is a player, display the message on the screen.
-          sendDataToServer.txt({
-            typ: 'fad',
-            text: hadrons.get(spriteKey).txt,
-            targetPlayerId: hadrons.get(obstacleSpriteKey)?.id,
-          });
-        }
-      } else if (hadrons.get(obstacleSpriteKey)?.typ === 'message') {
-        // Message was hit by something that I control.
+    } else if (obstacleSpriteKey) {
+      if (hadrons.has(obstacleSpriteKey)) {
+        // Obstacle can linger, and collide, after there hadron is destroyed, but we do not need to act on them anymore.
+        // Sprite to Sprite interactions
         if (
-          hadrons.get(spriteKey)?.typ !== 'player' &&
-          hadrons.get(spriteKey)?.own === hadrons.get(obstacleSpriteKey)?.own
+          hadrons.get(spriteKey)?.typ === 'message' &&
+          hadrons.get(spriteKey)?.txt !== undefined
         ) {
-          // Anything that is NOT a player, and is owned by the same owner as the message
-          // will de-spawn
-          // and destroy the message.
-          sendDataToServer.destroyHadron(spriteKey);
-          sendDataToServer.destroyHadron(obstacleSpriteKey);
-        }
-      } else if (
-        hadrons.get(spriteKey)?.typ === 'spell' &&
-        spells[hadrons.get(spriteKey)?.sub].type === 'damage' &&
-        hadrons.get(obstacleSpriteKey)?.typ === 'spell' &&
-        spells[hadrons.get(obstacleSpriteKey)?.sub].type === 'damage'
-      ) {
-        if (
-          hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
-        ) {
-          // If two damage spells, owned by different players, collide, they destroy each other.
-          sendDataToServer.destroyHadron(spriteKey);
-          sendDataToServer.destroyHadron(obstacleSpriteKey);
-        }
-        // A player's own spells hitting each other is entirely ignored.
-      } else if (
-        hadrons.get(spriteKey)?.typ === 'spell' &&
-        spells[hadrons.get(spriteKey)?.sub].type === 'damage' &&
-        hadrons.get(obstacleSpriteKey)?.typ === 'player'
-      ) {
-        if (
-          hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
-        ) {
-          // If a damage spell hits a player that is not the owner:
-          const dps = hadrons.get(spriteKey)?.dps;
-          // 1. De-spawn the spell.
-          sendDataToServer.destroyHadron(spriteKey);
-          // 2. Apply damage to the player.
-          let amount = 1;
-          if (
-            dps &&
-            // eslint-disable-next-line no-restricted-globals
-            !isNaN(dps)
-          ) {
-            amount *= dps;
-          }
-          sendDataToServer.damageHadron({ id: obstacleSpriteKey, amount });
-        }
-        // A player's spells hitting themselves is entirely ignored.
-      } else if (
-        hadrons.get(spriteKey)?.typ === 'spell' &&
-        hadrons.get(obstacleSpriteKey)?.typ === 'quark'
-      ) {
-        if (
-          hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
-        ) {
-          if (hadrons.get(obstacleSpriteKey)?.flv === 'NPC') {
-            // If a spell hits an NPC...
-            // Destroy the spell hadron
-            sendDataToServer.destroyHadron(spriteKey);
-            // Render damage to the NPC
-            sendDataToServer.damageHadron({
-              id: obstacleSpriteKey,
-              // TODO: Amount should probably be stored in the sprite data and gotten from the spell?
-              amount: 1,
+          // Messages - A message sprite that we are tracking has collided with something.
+          if (hadrons.get(obstacleSpriteKey)?.typ === 'player') {
+            // If it is a player, display the message on the screen.
+            sendDataToServer.txt({
+              typ: 'fad',
+              text: hadrons.get(spriteKey).txt,
+              targetPlayerId: hadrons.get(obstacleSpriteKey)?.id,
             });
-          } else if (hadrons.get(obstacleSpriteKey)?.flv === 'Item') {
-            // Destroy the spell hadron
+          }
+        } else if (hadrons.get(obstacleSpriteKey)?.typ === 'message') {
+          // Message was hit by something that I control.
+          if (
+            hadrons.get(spriteKey)?.typ !== 'player' &&
+            hadrons.get(spriteKey)?.own === hadrons.get(obstacleSpriteKey)?.own
+          ) {
+            // Anything that is NOT a player, and is owned by the same owner as the message
+            // will de-spawn
+            // and destroy the message.
             sendDataToServer.destroyHadron(spriteKey);
-            // Render damage to the Item if it has health (not all do)
-            if (hadrons.get(obstacleSpriteKey)?.hlt) {
+            sendDataToServer.destroyHadron(obstacleSpriteKey);
+          }
+        } else if (
+          hadrons.get(spriteKey)?.typ === 'spell' &&
+          spells[hadrons.get(spriteKey)?.sub].type === 'damage' &&
+          hadrons.get(obstacleSpriteKey)?.typ === 'spell' &&
+          spells[hadrons.get(obstacleSpriteKey)?.sub].type === 'damage'
+        ) {
+          if (
+            hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
+          ) {
+            // If two damage spells, owned by different players, collide, they destroy each other.
+            sendDataToServer.destroyHadron(spriteKey);
+            sendDataToServer.destroyHadron(obstacleSpriteKey);
+          }
+          // A player's own spells hitting each other is entirely ignored.
+        } else if (
+          hadrons.get(spriteKey)?.typ === 'spell' &&
+          spells[hadrons.get(spriteKey)?.sub].type === 'damage' &&
+          hadrons.get(obstacleSpriteKey)?.typ === 'player'
+        ) {
+          if (
+            hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
+          ) {
+            // If a damage spell hits a player that is not the owner:
+            const dps = hadrons.get(spriteKey)?.dps;
+            // 1. De-spawn the spell.
+            sendDataToServer.destroyHadron(spriteKey);
+            // 2. Apply damage to the player.
+            let amount = 1;
+            if (
+              dps &&
+              // eslint-disable-next-line no-restricted-globals
+              !isNaN(dps)
+            ) {
+              amount *= dps;
+            }
+            sendDataToServer.damageHadron({ id: obstacleSpriteKey, amount });
+          }
+          // A player's spells hitting themselves is entirely ignored.
+        } else if (
+          hadrons.get(spriteKey)?.typ === 'spell' &&
+          hadrons.get(obstacleSpriteKey)?.typ === 'quark'
+        ) {
+          if (
+            hadrons.get(spriteKey)?.own !== hadrons.get(obstacleSpriteKey)?.own
+          ) {
+            if (hadrons.get(obstacleSpriteKey)?.flv === 'NPC') {
+              // If a spell hits an NPC...
+              // Destroy the spell hadron
+              sendDataToServer.destroyHadron(spriteKey);
+              // Render damage to the NPC
               sendDataToServer.damageHadron({
                 id: obstacleSpriteKey,
                 // TODO: Amount should probably be stored in the sprite data and gotten from the spell?
                 amount: 1,
               });
+            } else if (hadrons.get(obstacleSpriteKey)?.flv === 'Item') {
+              // Destroy the spell hadron
+              sendDataToServer.destroyHadron(spriteKey);
+              // Render damage to the Item if it has health (not all do)
+              if (hadrons.get(obstacleSpriteKey)?.hlt) {
+                sendDataToServer.damageHadron({
+                  id: obstacleSpriteKey,
+                  // TODO: Amount should probably be stored in the sprite data and gotten from the spell?
+                  amount: 1,
+                });
+              }
             }
+            // Spells hitting Non-NPC Quarks are ignored
+          } else if (hadrons.get(obstacleSpriteKey)?.flv === 'Item') {
+            // Shooting your own items has no affect,
+            // but the spells go away.
+            // Destroy the spell hadron
+            sendDataToServer.destroyHadron(spriteKey);
           }
-          // Spells hitting Non-NPC Quarks are ignored
-        } else if (hadrons.get(obstacleSpriteKey)?.flv === 'Item') {
-          // Shooting your own items has no affect,
-          // but the spells go away.
-          // Destroy the spell hadron
-          sendDataToServer.destroyHadron(spriteKey);
+          // An NPC's spell hitting itself is ignored.
+        } else if (
+          hadrons.get(spriteKey)?.typ === 'quark' &&
+          hadrons.get(obstacleSpriteKey)?.typ === 'spell'
+        ) {
+          // NPC hitting a spell is ignored.
+          // Spells hit NPCs, but if an NPC hits a spell, we don't actually care, as the Spell->NPC already covered the action.
+        } else if (
+          hadrons.get(obstacleSpriteKey)?.typ === 'player' &&
+          hadrons.get(spriteKey)?.flv === 'NPC' &&
+          hadrons.get(spriteKey)?.dpc
+        ) {
+          // If an NPC or Item is set to damage players on contact, do so.
+          const amount = hadrons.get(spriteKey)?.dpc;
+          // console.log(amount / 100);
+          sendDataToServer.damageHadron({ id: obstacleSpriteKey, amount });
+        } else {
+          // Anything else just passes through
+          // console.log(
+          //   hadrons.get(spriteKey)?.typ,
+          // hadrons.get(obstacleSpriteKey)?.typ,
+          // hadrons.get(obstacleSpriteKey)?.flv,
+          // spriteKey,
+          // sprite,
+          // obstacleLayerName,
+          // obstacleSpriteKey,
+          // obstacleSprite,
+          // teleportLayerProperties,
+          // );
         }
-        // An NPC's spell hitting itself is ignored.
-      } else if (
-        hadrons.get(spriteKey)?.typ === 'quark' &&
-        hadrons.get(obstacleSpriteKey)?.typ === 'spell'
-      ) {
-        // NPC hitting a spell is ignored.
-        // Spells hit NPCs, but if an NPC hits a spell, we don't actually care, as the Spell->NPC already covered the action.
-      } else if (
-        hadrons.get(obstacleSpriteKey)?.typ === 'player' &&
-        hadrons.get(spriteKey)?.flv === 'NPC' &&
-        hadrons.get(spriteKey)?.dpc
-      ) {
-        // If an NPC or Item is set to damage players on contact, do so.
-        const amount = hadrons.get(spriteKey)?.dpc;
-        // console.log(amount / 100);
-        sendDataToServer.damageHadron({ id: obstacleSpriteKey, amount });
-      } else {
-        // Anything else just passes through
-        // console.log(
-        //   hadrons.get(spriteKey)?.typ,
-        // hadrons.get(obstacleSpriteKey)?.typ,
-        // hadrons.get(obstacleSpriteKey)?.flv,
-        // spriteKey,
-        // sprite,
-        // obstacleLayerName,
-        // obstacleSpriteKey,
-        // obstacleSprite,
-        // teleportLayerProperties,
-        // );
       }
     } else if (playerObject.enableDebug) {
       // You should never get here unless you are testing out new things.
-      // NOTE: IF in doubt, copy this to BEFORE the if/else chain to see what is happening.
+      // IF in doubt, copy this to BEFORE the if/else chain to see what is happening.
       console.error(
         '--------------',
         `\n${hadrons.get(spriteKey)?.typ} sprite ${spriteKey}`,
@@ -239,7 +241,7 @@ function spriteCollisionHandler({
         `${teleportLayerProperties ? `\nteleportLayerProperties:` : ''}`,
       );
       if (teleportLayerProperties) {
-        console.log(teleportLayerProperties);
+        console.error(teleportLayerProperties);
       }
     }
   }
